b44c1cd394b47e7dfd69797601766ec60f43aded
Dejan Milicic <dejan.milicic@gmail.com>
2020-04-18 13:00:54 +0200

    start of an attempt to define ravendb index in f#

--- SharpRaven/SharpRaven/Infrastructure.fs
+++ SharpRaven/SharpRaven/Infrastructure.fs
@@ -1,9 +1,39 @@
 ﻿module Infrastructure
 
 open Raven.Client.Documents
+open Raven.Client.Documents.Indexes
+open System.Reflection
+open SharpRaven
 
 module Persistence =
+
+  type UserReduceResult() =
+    member val Id : string = null with get, set
+    member val Name : string = null with get, set
+
+  type UsersByName() as this =
+    inherit AbstractIndexCreationTask<User, UserReduceResult>()
+
+    let map = <@ fun (users:seq<User>) -> 
+                    query { 
+                        for u in users do
+                        select (UserReduceResult(Id = u.Id, Name = u.Name))
+                    } @>
+    let reduce = <@ fun (results:seq<UserReduceResult>) ->
+                        query {
+                            for result in results do
+                            groupBy result into g
+                            select (UserReduceResult(Id = g.Key.Id, Name = g.Key.Name))
+                        } @>
+    do
+        this.Map <- Expr.ToIndexExpression map
+        this.Reduce <- Expr.ToIndexExpression reduce
+
+
+
+
   let configure (store : IDocumentStore) =
+      IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store)
       store.Initialize () |> ignore
 
   let Store = 
