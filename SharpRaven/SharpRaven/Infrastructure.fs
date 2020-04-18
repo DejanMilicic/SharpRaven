e9939f47cff661a9deb181da8cfbb0ffebc67a74
Dejan Milicic <dejan.milicic@gmail.com>
2020-04-18 14:21:30 +0200

    further attempts at defining index

--- SharpRaven/SharpRaven/Infrastructure.fs
+++ SharpRaven/SharpRaven/Infrastructure.fs
@@ -4,8 +4,50 @@ open Raven.Client.Documents
 open Raven.Client.Documents.Indexes
 open System.Reflection
 open SharpRaven
+open System.Linq.Expressions
+open Microsoft.FSharp.Quotations
+open System
 
 module Persistence =
+
+  let rec translateSimpleExpr expr =
+    match expr with
+    | Patterns.Var(var) ->
+        // Variable access
+        Expression.Variable(var.Type, var.Name) :> Expression
+    | Patterns.PropertyGet(Some inst, pi, []) ->
+        // Getter of an instance property
+        let instExpr = translateSimpleExpr inst
+        Expression.Property(instExpr, pi) :> Expression
+    | Patterns.Call(Some inst, mi, args) ->
+        // Method call - translate instance & arguments recursively
+        let argsExpr = Seq.map translateSimpleExpr args
+        let instExpr = translateSimpleExpr inst
+        Expression.Call(instExpr, mi, argsExpr) :> Expression
+    | Patterns.Call(None, mi, args) ->
+        // Static method call - no instance
+        let argsExpr = Seq.map translateSimpleExpr args
+        Expression.Call(mi, argsExpr) :> Expression
+    | _ -> failwith "not supported"
+
+  /// Translates a simple F# quotation to a lambda expression
+  let translateLambda (expr:Expr<'T -> 'R>) =
+    match expr with
+    | Patterns.Lambda(v, body) ->
+        // Build LINQ style lambda expression
+        let bodyExpr = translateSimpleExpr body
+        let paramExpr = Expression.Parameter(v.Type, v.Name)
+        Expression.Lambda<Func<'T, 'R>>(paramExpr)
+    | _ -> failwith "not supported"
+
+  let ToIndexExpression (expr:Expr<seq<'a> -> seq<'b>>) =
+      match expr with
+      | Patterns.Lambda(v, body) ->
+          // Build LINQ style lambda expression
+          let bodyExpr = translateSimpleExpr body
+          let paramExpr = Expression.Parameter(v.Type, v.Name)
+          Expression.Lambda<Func<seq<'a>, System.Collections.IEnumerable>>(bodyExpr, paramExpr)
+      | _ -> failwith "not supported"
 
   type UserReduceResult() =
     member val Id : string = null with get, set
@@ -25,11 +67,10 @@ module Persistence =
                             groupBy result into g
                             select (UserReduceResult(Id = g.Key.Id, Name = g.Key.Name))
                         } @>
+
     do
-        this.Map <- Expr.ToIndexExpression map
-        this.Reduce <- Expr.ToIndexExpression reduce
-
-
+        this.Map <- ToIndexExpression map
+        this.Reduce <- ToIndexExpression reduce
 
 
   let configure (store : IDocumentStore) =
