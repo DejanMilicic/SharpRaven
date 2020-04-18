69d374f9b136b9e4e2a45695692d9e5cac842845
Dejan Milicic <dejan.milicic@gmail.com>
2020-04-18 22:38:14 +0200

    Altermative way to create index

--- SharpRaven/SharpRaven/Infrastructure.fs
+++ SharpRaven/SharpRaven/Infrastructure.fs
@@ -3,79 +3,38 @@
 open Raven.Client.Documents
 open Raven.Client.Documents.Indexes
 open System.Reflection
-open SharpRaven
-open System.Linq.Expressions
-open Microsoft.FSharp.Quotations
-open System
+open System.Collections.Generic
 
 module Persistence =
 
-  let rec translateSimpleExpr expr =
-    match expr with
-    | Patterns.Var(var) ->
-        // Variable access
-        Expression.Variable(var.Type, var.Name) :> Expression
-    | Patterns.PropertyGet(Some inst, pi, []) ->
-        // Getter of an instance property
-        let instExpr = translateSimpleExpr inst
-        Expression.Property(instExpr, pi) :> Expression
-    | Patterns.Call(Some inst, mi, args) ->
-        // Method call - translate instance & arguments recursively
-        let argsExpr = Seq.map translateSimpleExpr args
-        let instExpr = translateSimpleExpr inst
-        Expression.Call(instExpr, mi, argsExpr) :> Expression
-    | Patterns.Call(None, mi, args) ->
-        // Static method call - no instance
-        let argsExpr = Seq.map translateSimpleExpr args
-        Expression.Call(mi, argsExpr) :> Expression
-    | _ -> failwith "not supported"
 
-  /// Translates a simple F# quotation to a lambda expression
-  let translateLambda (expr:Expr<'T -> 'R>) =
-    match expr with
-    | Patterns.Lambda(v, body) ->
-        // Build LINQ style lambda expression
-        let bodyExpr = translateSimpleExpr body
-        let paramExpr = Expression.Parameter(v.Type, v.Name)
-        Expression.Lambda<Func<'T, 'R>>(paramExpr)
-    | _ -> failwith "not supported"
 
-  let ToIndexExpression (expr:Expr<seq<'a> -> seq<'b>>) =
-      match expr with
-      | Patterns.Lambda(v, body) ->
-          // Build LINQ style lambda expression
-          let bodyExpr = translateSimpleExpr body
-          let paramExpr = Expression.Parameter(v.Type, v.Name)
-          Expression.Lambda<Func<seq<'a>, System.Collections.IEnumerable>>(bodyExpr, paramExpr)
-      | _ -> failwith "not supported"
-
-  type UserReduceResult() =
-    member val Id : string = null with get, set
-    member val Name : string = null with get, set
-
-  type UsersByName() as this =
-    inherit AbstractIndexCreationTask<User, UserReduceResult>()
-
-    let map = <@ fun (users:seq<User>) -> 
-                    query { 
-                        for u in users do
-                        select (UserReduceResult(Id = u.Id, Name = u.Name))
-                    } @>
-    let reduce = <@ fun (results:seq<UserReduceResult>) ->
-                        query {
-                            for result in results do
-                            groupBy result into g
-                            select (UserReduceResult(Id = g.Key.Id, Name = g.Key.Name))
-                        } @>
-
+  type Users_ByName () as this =
+    inherit AbstractJavaScriptIndexCreationTask ()
     do
-        this.Map <- ToIndexExpression map
-        this.Reduce <- ToIndexExpression reduce
+      this.Maps <-
+        HashSet<string> ["
+        map('Users', function(u){
+          return {
+              Name: u.Name,
+              Count: 1
+          }
+        })
+        "]
+      this.Reduce <- "
+        groupBy(x => x.Name)
+        .aggregate(g => {
+            return {
+                Name: g.key,
+                Count: g.values.reduce((count, val) => val.Count + count, 0)
+            };
+        })
+      "
 
 
   let configure (store : IDocumentStore) =
-      IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store)
       store.Initialize () |> ignore
+      IndexCreation.CreateIndexes(Assembly.GetExecutingAssembly(), store)
 
   let Store = 
       let store = new DocumentStore ()
