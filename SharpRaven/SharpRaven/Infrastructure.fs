191fc017427fd84ae37eb88b65dec1c0d9fb22f9
Dejan Milicic <dejan.milicic@gmail.com>
2020-04-18 12:11:06 +0200

    ravendb connection correction

--- SharpRaven/SharpRaven/Infrastructure.fs
+++ SharpRaven/SharpRaven/Infrastructure.fs
@@ -6,9 +6,9 @@ module Persistence =
   let configure (store : IDocumentStore) =
       store.Initialize () |> ignore
 
-  let configureStore = 
+  let Store = 
       let store = new DocumentStore ()
-      store.Urls <- [|"http://127.0.0.1"|]
+      store.Urls <- [|"http://127.0.0.1:8080"|]
       store.Database <- "SharpRaven"
       configure store
       store
